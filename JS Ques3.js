f48bfc75c29a9f4f2cb7407d7f62b0a77585cb8d
Avanimaletha <125181112+Avanimaletha@users.noreply.github.com>
2024-09-29 23:13:51 +0530

    Create JS Ques3.js
    ys

---
+++ JS Ques3.js
@@ -0,0 +1,18 @@
+var expect = function (val) {
+    return {
+        toBe: (otherVal) => {
+            if (val !== otherVal) {
+                throw new Error("Not Equal");
+            } else {
+                return true;
+            }
+        },
+        notToBe: (otherVal) => {
+            if (val === otherVal) {
+                throw new Error("Equal");
+            } else {
+                return true;
+            }
+        },
+    };
+};
