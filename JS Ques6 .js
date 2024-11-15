b499039000c159b69189f0e1fb55e7d60f5e322c
Avanimaletha <125181112+Avanimaletha@users.noreply.github.com>
2024-11-16 02:26:22 +0530

    Create JS Ques6 .js
    gg

---
+++ JS Ques6 .js
@@ -0,0 +1,11 @@
+var filter = function(arr, fn) {
+    const ans =[] ;
+    for (const i in arr)
+    {
+        if(fn(arr[i],Number(i)))
+        {
+            ans.push(arr[i]);
+        }
+    }
+    return ans;
+};
